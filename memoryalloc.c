0b91ad6a8626004aafef46c0e22e075fdf14b8e5
Murtazah1 <mhx017@gmail.com>
2025-01-20 20:15:39 -0600

    malloc commit

--- memoryalloc.c
+++ memoryalloc.c
@@ -1,4 +1,36 @@
 #include "jun.h"
-printf("hello world");
+#include <unistd.h>
+#include <assert.h>
+#include <string.h>
+#include <sys/types.h>
+#include <stdio.h>
 
 
+
+
+void *custommalloc(size_t size){
+	// this accesses the top of the heap
+	void *top = sbrk(0);
+	// this changes the top of the heap to where size is
+	void *request = sbrk(size);
+	if (request == (void*) -1){
+		return NULL; // make sure there is no error
+	}
+	else{
+		assert(top == request); // make sure the thread is safe
+		return top;
+	}
+}
+
+
+int main(void){
+	void *ptr = custommalloc(sizeof(int));
+	if (ptr == NULL){
+		printf("Memory fail\n");
+		return 1;
+	}
+	else{
+		printf('%p',(void *)ptr);
+		return 0;
+	}
+}
